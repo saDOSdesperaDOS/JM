0a0c8e143b1b9ffac8973aea3fade3b75517496f
sadosdesperados <bms39rus@gmail.com>
2020-03-14 11:43:08 +0200

    added solution task228

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,12 +1,26 @@
-
 public class MainTest {
 
 	public static void main(String[] args) {
-
-		short a = 184;
-	}
+		/*
+		 Возвращает беззнаковый коэффициент деления 
+		 первого аргумента на второй, где каждый аргумент и 
+		 результат интерпретируются как беззнаковое значение.
+		 **/
+		char c = 'f';
+		System.out.println(Integer.bitCount(7));
+		System.out.println(Integer.toBinaryString(7));
+		System.out.println(Integer.bitCount(8));
+		System.out.println(Integer.toBinaryString(8));
+		System.out.println(Integer.bitCount(9));
+		System.out.println(Integer.toBinaryString(9));
+		System.out.println(Integer.bitCount(16));
+		System.out.println(Integer.toBinaryString(16));
+		System.out.println(Integer.bitCount(18));
+		System.out.println(Integer.toBinaryString(18));
+		System.out.println(Integer.bitCount(32));
+		System.out.println(Integer.toBinaryString(32));
+		System.out.println(Integer.bitCount(32));
+		System.out.println(Integer.toBinaryString(32));
 	
-	public static boolean doubleExpression(double a, double b, double c) {
-		return Math.abs(a+b-c) < 0.0001; 
 	}
 }
