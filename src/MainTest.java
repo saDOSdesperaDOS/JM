7404da5c509f40750b353497a4b019a7aedb900f
sadosdesperados <bms39rus@gmail.com>
2020-03-14 10:00:56 +0200

    Solution Task2110

---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+		short a = 184;
+	}
+	
+	public static boolean doubleExpression(double a, double b, double c) {
+		return Math.abs(a+b-c) < 0.0001; 
+	}
+}
