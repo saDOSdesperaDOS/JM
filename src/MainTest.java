494f118061cdeb0fb9b10c8cce362be71c08a39d
sadosdesperados <bms39rus@gmail.com>
2020-03-18 07:21:56 +0200

    Added solution Task2411. Implementation of merge sort

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,26 +1,11 @@
+import core.Task2411;
+
 public class MainTest {
 
 	public static void main(String[] args) {
-		/*
-		 Возвращает беззнаковый коэффициент деления 
-		 первого аргумента на второй, где каждый аргумент и 
-		 результат интерпретируются как беззнаковое значение.
-		 **/
-		char c = 'f';
-		System.out.println(Integer.bitCount(7));
-		System.out.println(Integer.toBinaryString(7));
-		System.out.println(Integer.bitCount(8));
-		System.out.println(Integer.toBinaryString(8));
-		System.out.println(Integer.bitCount(9));
-		System.out.println(Integer.toBinaryString(9));
-		System.out.println(Integer.bitCount(16));
-		System.out.println(Integer.toBinaryString(16));
-		System.out.println(Integer.bitCount(18));
-		System.out.println(Integer.toBinaryString(18));
-		System.out.println(Integer.bitCount(32));
-		System.out.println(Integer.toBinaryString(32));
-		System.out.println(Integer.bitCount(32));
-		System.out.println(Integer.toBinaryString(32));
+		for (int i : Task2411.mergeArrays(new int[] {0,2,2}, new int[] {1,3})) {
+			System.out.print(i + " ");
+		}
 	
 	}
 }
