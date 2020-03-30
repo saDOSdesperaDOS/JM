9cdff4303dec57ef39d3e45f6175de414584cd08
sadosdesperados <bms39rus@gmail.com>
2020-03-30 10:58:23 +0300

    added classes to task357

--- src/core/task357/Task357.java
+++ src/core/task357/Task357.java
@@ -3,17 +3,80 @@ package core.task357;
 public class Task357 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 	
 	public Label checkLabels(TextAnalyzer[] analyzers, String text) {
-		//“вой код здесь
 		return Label.OK;
 	}
 	
-    //“вой код здесь
+	abstract class KeywordAnalyzer  {
+		
+		protected abstract String[] getKeywords();
+		
+		protected abstract Label getLabel();
+	}
+	
+	class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+		
+		private String[] keywords;
+		
+		public SpamAnalyzer(String[] keywords) {
+			this.keywords = keywords;
+		}
 
-//
+		@Override
+		protected String[] getKeywords() {
+			// TODO Auto-generated method stub
+			return keywords;
+		}
+
+		@Override
+		protected Label getLabel() {
+			// TODO Auto-generated method stub
+			return Label.SPAM;
+		}
+		
+		@Override
+		public Label processText(String text) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	class NegativeTextAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+
+		@Override
+		protected String[] getKeywords() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected Label getLabel() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public Label processText(String text) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	class TooLongTextAnalyzer implements TextAnalyzer {
+
+		@Override
+		public Label processText(String text) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	
 
 }
