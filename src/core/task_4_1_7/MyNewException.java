da5a080b9894a24e9a4bd60193cf7dfab64504cb
sadosdesperados <bms39rus@gmail.com>
2020-04-01 10:37:32 +0300

    task_4_1_7

---
+++ src/core/task_4_1_7/MyNewException.java
@@ -0,0 +1,10 @@
+package core.task_4_1_7;
+
+public class MyNewException extends Throwable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/core/task_4_1_7/Solution_4_1_7.java
@@ -0,0 +1,10 @@
+package core.task_4_1_7;
+
+public class Solution_4_1_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
